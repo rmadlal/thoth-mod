3fe78efc20aff854f45aecc7c31ccd8a5dd684cd
rmadlal <ronmadlal@gmail.com>
2018-01-23 03:13:09 +0200

    Fix settings file writing on quit

--- ModifiedClasses/Legacy/ExitFade.cs
+++ ModifiedClasses/Legacy/ExitFade.cs
@@ -7,25 +7,13 @@ public class ExitFade : MonoBehaviour
 	// New
 	private void OnApplicationQuit()
 	{
-		string[] contents = new string[]
-		{
-			"Ver2   - do not modify this line",
-			"//VSync, 0 means off, 1 means on. Turning it off can minimize input latency but might introduce some screen tearing",
-			"1",
-			"//Anti-aliasing. 0 means off, 1 means on. AA is expensive so it is off by default",
-			"0",
-			"//Cheap lava optimization, 0 means off, 1 means on. If you experience low fps, turn it on",
-			"0",
-			"//Window size: width,height,fullscreen",
-			string.Join(",", new string[]
-			{
-				Screen.width.ToString(),
-				Screen.height.ToString(),
-				Screen.fullScreen.ToString()
-			}),
-			"//Debug",
-			Globals.showRoomDebugInfo.ToString()
-		};
-		File.WriteAllLines("settings.txt", contents);
+		string[] array = File.ReadAllLines("settings.txt");
+        array[8] = string.Join(",", new string[]
+        {
+            Screen.width.ToString(),
+            Screen.height.ToString(),
+            Screen.fullScreen.ToString()
+        });
+        File.WriteAllLines("settings.txt", array);
 	}
 }
